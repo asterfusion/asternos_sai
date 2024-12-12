22f0ec9a98d26a003f01bd8b2d6ecd72a16a9911
chenxu <chenxu@asterfusion.com>
2024-12-12 02:21:55 -0800

    [PTP]CXMPLS-298 support PTP multi-domain reviewer:yangyue
    Signed-off-by: chenxu <chenxu@asterfusion.com>

--- inc/saiptp.h
+++ inc/saiptp.h
@@ -34,19 +34,6 @@
  */
 
 /**
- * @brief Transport mode of PTP
- */
-typedef enum _sai_ptp_clock_transport_mode_t
-{
-    /** UDP IPv4 or IPv6 */
-    SAI_PTP_CLOCK_TRANSPORT_MODE_UDP,
-
-    /** Ethernet */
-    SAI_PTP_CLOCK_TRANSPORT_MODE_IEEE_802_3,
-
-} sai_ptp_clock_transport_mode_t;
-
-/**
  * @brief PTP domain mode of PTP
  */
 typedef enum _sai_ptp_domain_mode_t
@@ -58,22 +45,6 @@ typedef enum _sai_ptp_domain_mode_t
     SAI_PTP_DOMAIN_MODE_PTP_V2,
 
 } sai_ptp_domain_mode_t;
-
-/**
- * @brief Clock type of PTP domain
- */
-typedef enum _sai_ptp_domain_clock_type_t
-{
-    /** OC */
-    SAI_PTP_DOMAIN_CLOCK_TYPE_OC,
-
-    /** BC */
-    SAI_PTP_DOMAIN_CLOCK_TYPE_BC,
-
-    /** TC */
-    SAI_PTP_DOMAIN_CLOCK_TYPE_TC,
-
-} sai_ptp_domain_clock_type_t;
 
 /**
  * @brief Step type of PTP domain
@@ -101,74 +72,6 @@ typedef enum _sai_ptp_domain_delay_mesurement_type_t
 
 } sai_ptp_domain_delay_mesurement_type_t;
 
-typedef enum _sai_ptp_clock_attr_t
-{
-    /**
-     * @brief Start of attributes
-     */
-    SAI_PTP_CLOCK_ATTR_START,
-
-    /**
-     * @brief PTP packet transport mode
-     *
-     * @type sai_ptp_clock_transport_mode_t
-     * @flags MANDATORY_ON_CREATE | CREATE_AND_SET
-     */
-    SAI_PTP_CLOCK_ATTR_TRANSPORT_MODE = SAI_PTP_CLOCK_ATTR_START,
-
-    /**
-     * @brief The ether type of PTP
-     *
-     * @type sai_uint32_t
-     * @flags CREATE_ONLY
-     * @default 0
-     * @validonly SAI_PTP_CLOCK_ATTR_TRANSPORT_MODE == SAI_PTP_CLOCK_TRANSPORT_MODE_IEEE_802_3
-     */
-    SAI_PTP_CLOCK_ATTR_ETHER_TYPE_0,
-
-    /**
-     * @brief The ether type of PTP
-     *
-     * @type sai_uint32_t
-     * @flags CREATE_ONLY
-     * @default 0
-     * @validonly SAI_PTP_CLOCK_ATTR_TRANSPORT_MODE == SAI_PTP_CLOCK_TRANSPORT_MODE_IEEE_802_3
-     */
-    SAI_PTP_CLOCK_ATTR_ETHER_TYPE_1,
-
-    /**
-     * @brief Destination Port
-     *
-     * @type sai_uint32_t
-     * @flags CREATE_ONLY
-     * @default 0
-     * @validonly SAI_PTP_CLOCK_ATTR_TRANSPORT_MODE == SAI_PTP_CLOCK_TRANSPORT_MODE_UDP
-     */
-    SAI_PTP_CLOCK_ATTR_DST_PORT_0,
-
-    /**
-     * @brief Destination Port
-     *
-     * @type sai_uint32_t
-     * @flags CREATE_ONLY
-     * @default 0
-     * @validonly SAI_PTP_CLOCK_ATTR_TRANSPORT_MODE == SAI_PTP_CLOCK_TRANSPORT_MODE_UDP
-     */
-    SAI_PTP_CLOCK_ATTR_DST_PORT_1,
-
-    /**
-     * @brief End of attributes
-     */
-    SAI_PTP_CLOCK_ATTR_END,
-
-    /** Custom range base value */
-    SAI_PTP_CLOCK_ATTR_CUSTOM_RANGE_START = 0x10000000,
-
-    /** End of custom range base */
-    SAI_PTP_CLOCK_ATTR_CUSTOM_RANGE_END
-
-} sai_ptp_clock_attr_t;
-
 typedef enum _sai_ptp_domain_entry_attr_t
 {
     /**
@@ -185,22 +88,13 @@ typedef enum _sai_ptp_domain_entry_attr_t
     SAI_PTP_DOMAIN_ENTRY_ATTR_PTP_DOMAIN_MODE = SAI_PTP_DOMAIN_ENTRY_ATTR_START,
 
     /**
-     * @brief The clock type of PTP domain
+     * @brief Query/Configure Domain ID
      *
-     * @type sai_object_id_t
-     * @flags MANDATORY_ON_CREATE | CREATE_ONLY
-     * @objects SAI_OBJECT_TYPE_PTP_CLOCK
-     */
-    SAI_PTP_DOMAIN_ENTRY_ATTR_CLOCK_ID,
-
-    /**
-     * @brief Query/Configure list of Domains
-     *
-     * @type sai_u32_list_t
+     * @type sai_uint32_t
      * @flags CREATE_AND_SET
-     * @default empty
+     * @default 0
      */
-    SAI_PTP_DOMAIN_ENTRY_ATTR_PTP_DOMAIN_LIST,
+    SAI_PTP_DOMAIN_ENTRY_ATTR_PTP_DOMAIN_ID,
 
     /**
      * @brief End of attributes
@@ -214,62 +108,6 @@ typedef enum _sai_ptp_domain_entry_attr_t
     SAI_PTP_DOMAIN_ENTRY_ATTR_CUSTOM_RANGE_END
 
 } sai_ptp_domain_entry_attr_t;
-
-/**
- * @brief Create PTP clock.
- *
- * @param[out] ptp_clock_id PTP Clock id
- * @param[in] switch_id Switch id
- * @param[in] attr_count Number of attributes
- * @param[in] attr_list Value of attributes
- *
- * @return #SAI_STATUS_SUCCESS if operation is successful otherwise a different
- * error code is returned.
- */
-typedef sai_status_t (*sai_create_ptp_clock_fn)(
-        _Out_ sai_object_id_t *ptp_clock_id,
-        _In_ sai_object_id_t switch_id,
-        _In_ uint32_t attr_count,
-        _In_ const sai_attribute_t *attr_list);
-
-/**
- * @brief Remove PTP Clock.
- *
- * @param[in] ptp_clock_id PTP Clock id
- *
- * @return #SAI_STATUS_SUCCESS if operation is successful otherwise a different
- * error code is returned.
- */
-typedef sai_status_t (*sai_remove_ptp_clock_fn)(
-        _In_ sai_object_id_t ptp_clock_id);
-
-/**
- * @brief Set PTP clock attributes.
- *
- * @param[in] ptp_clock_id PTP Clock id
- * @param[in] attr Value of attribute
- *
- * @return #SAI_STATUS_SUCCESS if operation is successful otherwise a different
- * error code is returned.
- */
-typedef sai_status_t (*sai_set_ptp_clock_attribute_fn)(
-        _In_ sai_object_id_t ptp_clock_id,
-        _In_ const sai_attribute_t *attr);
-
-/**
- * @brief Get PTP clock attributes.
- *
- * @param[in] ptp_clock_id PTP Clock id
- * @param[in] attr_count Number of attributes
- * @param[inout] attr_list Value of attribute
- *
- * @return #SAI_STATUS_SUCCESS if operation is successful otherwise a different
- * error code is returned.
- */
-typedef sai_status_t (*sai_get_ptp_clock_attribute_fn)(
-        _In_ sai_object_id_t ptp_clock_id,
-        _In_ uint32_t attr_count,
-        _Inout_ sai_attribute_t *attr_list);
 
 /**
  * @brief Create PTP domain entry.
@@ -332,15 +170,10 @@ typedef sai_status_t (*sai_get_ptp_domain_entry_attribute_fn)(
  */
 typedef struct _sai_ptp_api_t
 {
-    sai_create_ptp_clock_fn                create_ptp_clock;
-    sai_remove_ptp_clock_fn                remove_ptp_clock;
-    sai_set_ptp_clock_attribute_fn         set_ptp_clock_attribute;
-    sai_get_ptp_clock_attribute_fn         get_ptp_clock_attribute;
     sai_create_ptp_domain_entry_fn         create_ptp_domain_entry;
     sai_remove_ptp_domain_entry_fn         remove_ptp_domain_entry;
     sai_set_ptp_domain_entry_attribute_fn  set_ptp_domain_entry_attribute;
     sai_get_ptp_domain_entry_attribute_fn  get_ptp_domain_entry_attribute;
-
 } sai_ptp_api_t;
 
 /**
--- inc/saitypes.h
+++ inc/saitypes.h
@@ -301,8 +301,7 @@ typedef enum _sai_object_type_t
     SAI_OBJECT_TYPE_ICMP_ECHO_SESSION        = 111,
     SAI_OBJECT_TYPE_VLAN_STACK               = 112,
     SAI_OBJECT_TYPE_OBJECT_GROUP             = 113,
-    SAI_OBJECT_TYPE_PTP_CLOCK                = 114,
-    SAI_OBJECT_TYPE_PTP_DOMAIN_ENTRY         = 115,
+    SAI_OBJECT_TYPE_PTP_DOMAIN_ENTRY         = 114,
 
     /** Must remain in last position */
     SAI_OBJECT_TYPE_MAX,
